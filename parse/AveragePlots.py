130b609a5805ce5ffd19e4f758d9d484fbada50c
Harper <hc2347@nyu.edu>
2019-07-29 10:50:12 -0700

    Updated AveragePlots.py

--- parse/AveragePlots.py
+++ parse/AveragePlots.py
@@ -3,8 +3,10 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 #%%
-csv_path = '/home/harper/enlace/enlace_xrd/parse/parsed.csv' #sys.argv[1] #
+csv_path = sys.argv[1] #
 org_df = pd.read_csv(csv_path)
+
+print(org_df)
 
 df = org_df.groupby(['concurrency']).mean()
 print(df)
@@ -12,7 +14,7 @@ print(df)
 #%%
 #Bar Graph of Actual/Expected Rates of Varying Concurrencies
 
-fig1, ax1 = plt.subplots(figsize=(6,6))
+fig1, ax1 = plt.subplots()
 
 width=50
 df = df.reset_index()
@@ -20,7 +22,8 @@ x = df['concurrency']
 rates_in_MB = df['rates']
 exp_rates_in_MB = df['exp rates']
 rates_in_Mbps = [rate*8 for rate in rates_in_MB]
-exp_rates = [exp_rate*8 for exp_rate in exp_rates_in_MB]
+exp_rates = [expect_rate*8 for expect_rate in exp_rates_in_MB]
+exp_rate = exp_rates[0]
 
 rects1 = ax1.bar(x - width/2, rates_in_Mbps, width, label='Actual Rate')
 rects2 = ax1.bar(x + width/2, exp_rates, width, label='Expected Rate')
@@ -34,7 +37,7 @@ ax1.set_title('Comparison of Actual vs Expected Rates \n')
 ax1.set_xticks(x)
 ax1.set_xticklabels(x)
 
-#plt.grid(b=True, which='major',color='#999999',linestyle='-')
+plt.grid(b=True, which='major',color='#999999',linestyle='-')
 plt.minorticks_on()
 plt.grid(b=True,which='minor',color='#999999',linestyle='-',alpha=0.2)
 #ax1.tick_params(labelbottom = False)
@@ -47,7 +50,7 @@ print("Saved plot for rate comparisons")
 #%%
 #Plot for percent of expected rate achieved
 
-fig2, ax2 = plt.subplots(figsize=(5,5))
+fig2, ax2 = plt.subplots()
 
 percent_rate = [(rate/exp)*100 for rate,exp in zip(rates_in_Mbps,exp_rates)]
 
@@ -71,34 +74,38 @@ print("Saved plot for percent of expected rate achieved.")
 #%%
 #Plot for the number of failed jobs
 
-fig3, ax3 = plt.subplots(figsize=(5,5))
+fig3, ax3 = plt.subplots()
 
 width=50
 
-empty = df['failed']
-per_fail = (empty/(x*5))*100
+empty = org_df['failed']
+list_empty = list(empty)
+print(list_empty)
 
-emptybar = ax3.bar(x, per_fail, width,label="Failed Jobs")
-
-ax3.set_xlabel('Concurrency')
-ax3.set_ylabel('Percent Failed')
-ax3.set_title('Percent of Failed Jobs\n')
-ax3.set_xticks(x)
-ax3.set_xticklabels(x)
-#ax1.tick_params(labelbottom = False)
-ax3.legend()
-
-for a,b in zip(x,per_fail):
-       plt.text(a,b,str(round(b,2)))
-
-plt.text(0.2,0.1,'Average of 4 runs at a rate of ' + str(exp_rate*8) + 'Mbps',fontsize = 11, transform=plt.gcf().transFigure)
-
-plt.savefig("Percent_of_Failed_Jobs.png")
-print("Saved plot for percent of jobs failed.")
+if sum(list_empty) == 0:
+       print("No failed jobs")
+else:
+       emptybar = ax3.bar(x, list_empty, width,label="Failed Jobs")
+       
+       ax3.set_xlabel('Concurrency')
+       ax3.set_ylabel('Number Failed')
+       ax3.set_title('Average Number of Failed Jobs\n')
+       ax3.set_xticks(x)
+       ax3.set_xticklabels(x)
+       #ax1.tick_params(labelbottom = False)
+       ax3.legend()
+       
+       for a,b in zip(x,empty):
+              plt.text(a,b,str(round(b,2)))
+              
+       plt.text(0.2,0.1,'Average of 4 runs at a rate of ' + str(exp_rate*8) + 'Mbps',fontsize = 11, transform=plt.gcf().transFigure)
+       
+       plt.savefig("Average_Number_of_Failed_Jobs.png")
+       print("Saved plot for number of jobs failed.")
 #%%
 #Plot for the average number of jobs hosted by a slot
 
-fig4, ax4 = plt.subplots(figsize=(5,5))
+fig4, ax4 = plt.subplots()
 
 width=50
 
